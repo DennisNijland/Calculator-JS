685aae98bd205b3e89b88b6daa703b91798ef75d
Dennis Nijland <Dennis Nijland>
2016-12-16 11:23:28 +0100

    no message

--- js/script.js
+++ js/script.js
@@ -1,4 +1,48 @@
 function add1toregister (parms){
-    register.innerHTML =+ "1";
+    register.innerHTML += "1";
 }
 document.getElementById("btn1").addEventListener("click", add1toregister);
+
+function add2toregister (parms){
+    register.innerHTML += "2";
+}
+document.getElementById("btn2").addEventListener("click", add2toregister);
+
+function add3toregister (parms){
+    register.innerHTML += "3";
+}
+document.getElementById("btn3").addEventListener("click", add3toregister);
+
+function add4toregister (parms){
+    register.innerHTML += "4";
+}
+document.getElementById("btn4").addEventListener("click", add4toregister);
+
+function add5toregister (parms){
+    register.innerHTML += "5";
+}
+document.getElementById("btn5").addEventListener("click", add5toregister);
+
+function add6toregister (parms){
+    register.innerHTML += "6";
+}
+document.getElementById("btn6").addEventListener("click", add6toregister);
+
+function add7toregister (parms){
+    register.innerHTML += "7";
+}
+document.getElementById("btn7").addEventListener("click", add7toregister);
+
+function add8toregister (parms){
+    register.innerHTML += "8";
+}
+document.getElementById("btn8").addEventListener("click", add8toregister);
+
+function add9toregister (parms){
+    register.innerHTML += "9";
+}
+document.getElementById("btn9").addEventListener("click", add9toregister);
+
+
+
+
