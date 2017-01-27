36eb395c96387c2813e8d2e8c5a5a3d2c9231f58
DENNIS-PC\Dennis <dennisnijland84@gmail.com>
2017-01-27 10:01:50 +0100

    Clear button toegevoegd

--- js/script.js
+++ js/script.js
@@ -1,48 +1,20 @@
-function add1toregister (parms){
-    register.innerHTML += "1";
+var register = document.getElementById('register'); 
+
+function cls (){
+	register.innerHTML = "";
 }
-document.getElementById("btn1").addEventListener("click", add1toregister);
-
-function add2toregister (parms){
-    register.innerHTML += "2";
+function click (parms){
+    register.innerHTML += parms.target.innerHTML;
 }
-document.getElementById("btn2").addEventListener("click", add2toregister);
 
-function add3toregister (parms){
-    register.innerHTML += "3";
-}
-document.getElementById("btn3").addEventListener("click", add3toregister);
-
-function add4toregister (parms){
-    register.innerHTML += "4";
-}
-document.getElementById("btn4").addEventListener("click", add4toregister);
-
-function add5toregister (parms){
-    register.innerHTML += "5";
-}
-document.getElementById("btn5").addEventListener("click", add5toregister);
-
-function add6toregister (parms){
-    register.innerHTML += "6";
-}
-document.getElementById("btn6").addEventListener("click", add6toregister);
-
-function add7toregister (parms){
-    register.innerHTML += "7";
-}
-document.getElementById("btn7").addEventListener("click", add7toregister);
-
-function add8toregister (parms){
-    register.innerHTML += "8";
-}
-document.getElementById("btn8").addEventListener("click", add8toregister);
-
-function add9toregister (parms){
-    register.innerHTML += "9";
-}
-document.getElementById("btn9").addEventListener("click", add9toregister);
-
-
-
-
+document.getElementById("btn1").addEventListener("click", click);
+document.getElementById("btn2").addEventListener("click", click);
+document.getElementById("btn3").addEventListener("click", click);
+document.getElementById("btn4").addEventListener("click", click);
+document.getElementById("btn5").addEventListener("click", click);
+document.getElementById("btn6").addEventListener("click", click);
+document.getElementById("btn7").addEventListener("click", click);
+document.getElementById("btn8").addEventListener("click", click);
+document.getElementById("btn9").addEventListener("click", click);
+document.getElementById("btn0").addEventListener("click", click);
+document.getElementById("btnc").addEventListener("click", cls);
