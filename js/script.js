6263e1b147ea868eef8d7bbc445f4937e5d44d8c
DENNIS-PC\Dennis <99028292@mydavinci.nl>
2017-01-27 10:20:34 +0100

    Punt button werkend

--- js/script.js
+++ js/script.js
@@ -1,11 +1,17 @@
 var register = document.getElementById('register'); 
-
+//Function leegmaken register
 function cls (){
 	register.innerHTML = "";
 }
+
+//Function nummer toevoegen aan register
 function click (parms){
     register.innerHTML += parms.target.innerHTML;
 }
+
+//Operator function
+
+
 
 document.getElementById("btn1").addEventListener("click", click);
 document.getElementById("btn2").addEventListener("click", click);
@@ -17,4 +23,5 @@ document.getElementById("btn7").addEventListener("click", click);
 document.getElementById("btn8").addEventListener("click", click);
 document.getElementById("btn9").addEventListener("click", click);
 document.getElementById("btn0").addEventListener("click", click);
+document.getElementById("dot").addEventListener("click", click);
 document.getElementById("btnc").addEventListener("click", cls);
